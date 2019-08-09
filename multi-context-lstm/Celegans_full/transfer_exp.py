ebeb628530a2abc841764b4fbc1209ec670804ce
Mohit Goyal <mohit@idpg-serv-01.csl.illinois.edu>
2019-08-09 12:43:12 -0500

    Celegans Full Model Fixed

--- multi-context-lstm/Celegans_full/transfer_exp.py
+++ multi-context-lstm/Celegans_full/transfer_exp.py
@@ -1,12 +1,13 @@
+from __future__ import print_function
 import numpy as np
-import os
+import keras
+import os 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
-import keras
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential, Model
-from keras.layers import Dense, Bidirectional, Input, add, concatenate, Lambda
+from keras.layers import Dense, Bidirectional, Input, add, concatenate
 from keras.layers import LSTM, Flatten, Conv1D, LocallyConnected1D, CuDNNLSTM, CuDNNGRU, MaxPooling1D, GlobalAveragePooling1D, GlobalMaxPooling1D
 from keras.models import *
 from keras.layers import *
@@ -28,6 +29,7 @@ import os
 from keras.callbacks import CSVLogger
 from keras import backend as K
 from clr import LRFinder, OneCycleLR
+from utils import *
 
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
@@ -71,25 +73,18 @@ class DataGenerator(keras.utils.Sequence):
     def __data_generation(self, list_IDs_temp):
 
         X = self.inputs[list_IDs_temp]
-        if self.use_model == False:
-            y = self.labels[list_IDs_temp]
-            true_lab = keras.utils.to_categorical(y, num_classes=self.n_classes)
-            return X, true_lab
-        else:
-            y = self.labels[list_IDs_temp]
-            true_lab = keras.utils.to_categorical(y, num_classes=self.n_classes)
-            y = self.model.predict(X, batch_size=len(X))
-            # features = self.intermediate_layer_model.predict(X, batch_size=len(X))
-            return X, [y, true_lab, y]
+        y = self.labels[list_IDs_temp]
+        true_lab = keras.utils.to_categorical(y, num_classes=self.n_classes)
+        return X, true_lab
 
 def res_block(inp, units=512, activation='relu'):
-    res = inp
+    x = res = inp
 
-    x = BatchNormalization()(res)
+    # x = BatchNormalization()(res)
     x = Activation(activation)(x)
     x = Dense(units)(x)
 
-    x = BatchNormalization()(x)
+    # x = BatchNormalization()(x)
     x = Activation(activation)(x)
     x = Dense(units)(x)
 
@@ -145,12 +140,11 @@ def generate_single_output_data(series,batch_size,time_steps):
         
 def fit_model(X, Y, bs, nb_epoch, student, teacher):
     y = Y
-    decayrate = 2.0/(len(Y) // bs)
-    optim = keras.optimizers.Adam(lr=5e-3, beta_1=0.9, beta_2=0.999, epsilon=None, amsgrad=False, decay=decayrate, clipnorm=0.1)
-    # optim = add_gradient_noise(keras.optimizers.RMSprop)(noise_eta=0.01)
+    decayrate = 64*2.0/(len(Y) // bs)
+    optim = keras.optimizers.Adam(lr=5e-4, beta_1=0.9, beta_2=0.999, epsilon=None, decay=decayrate, amsgrad=False)
     student.compile(loss={'1': loss_fn}, optimizer=optim, metrics=['acc'])
-    checkpoint = ModelCheckpoint("directexp", monitor='loss', verbose=1, save_best_only=True, mode='min', save_weights_only=True)
-    csv_logger = CSVLogger("log_directstudentexp.csv", append=True, separator=';')
+    checkpoint = ModelCheckpoint("modeltransferexp", monitor='loss', verbose=1, save_best_only=True, mode='min', save_weights_only=True)
+    csv_logger = CSVLogger("log_transferexp.csv", append=True, separator=';')
     early_stopping = EarlyStopping(monitor='loss', mode='min', min_delta=0.005, patience=3, verbose=1)
 
     # lr_manager = OneCycleLR(10**(-1.2), bs, len(y), nb_epoch, end_percentage=0.3)
@@ -158,12 +152,30 @@ def fit_model(X, Y, bs, nb_epoch, student, teacher):
     callbacks_list = [checkpoint, csv_logger, early_stopping]
 
     indices = np.arange(X.shape[-1]).reshape(1, -1)
-    train_gen = DataGenerator(X, y, teacher, bs, n_classes, shuffle=True, use_model=False)
+    train_gen = DataGenerator(X, y, teacher, bs, n_classes, shuffle=False)
     # val_gen = DataGenerator(X, y, teacher, bs, n_classes, True, use_model=False)
 
-    student.fit_generator(train_gen, epochs=nb_epoch, verbose=1, callbacks=callbacks_list, use_multiprocessing=True, workers=0)
+    # student.ifit_generator(train_gen, epochs=1, verbose=1, callbacks=callbacks_list, use_multiprocessing=True, workers=0
+    # X = X[100000:]
+    # y = y[100000:]
+    i = 0
+    loss_list = []
+    for batch_x, batch_y in iterate_minibatches(X, y, bs, n_classes, shuffle=False):
+	i = i+1
+	out = student.test_on_batch(batch_x, batch_y)
+        loss_list.append(out[0])
+	out = student.train_on_batch(batch_x, batch_y)
+	if i%10==0:
+	    sys.stdout.flush()
+	    print('Batch {}/{} Loss = {:4f}'.format(i, len(y)//bs, np.mean(loss_list)), end='\r')
+        if i%100000==0:
+            np.save('transfer_explosslist', np.array(loss_list))
+	
+    print('Training Complete Batch {}/{} Loss = {:4f}'.format(i, len(y)//bs, np.mean(loss_list)))
+    np.save('transfer_explosslist', np.array(loss_list))
 
-batch_size=2048
+
+batch_size=64
 sequence_length=64
 num_epochs=10
 noise = 0.0
@@ -173,50 +185,35 @@ def my_shape(input_shape):
     print(np.ceil(input_shape[1]/jump))
     return tuple((input_shape[0],int(np.ceil(float(input_shape[1])/jump)),input_shape[2]))
 
+
 def biGRU_big(bs,time_steps,alphabet_size):
   inputs_bits = Input(shape=(time_steps,))
-  x  = Embedding(alphabet_size, 8,)(inputs_bits)
+  x   = Embedding(alphabet_size, 8,)(inputs_bits)
   x = Bidirectional(CuDNNGRU(32, stateful=False, return_sequences=True))(x)
   x = Bidirectional(CuDNNGRU(32, stateful=False, return_sequences=True))(x)
-  x = Lambda(lambda tensor: tensor[:,::-jump,:][:, ::-1, :], output_shape=my_shape)(x)
-  flat = Flatten()(x)  
+  x = Lambda(lambda tensor: tensor[:,::-jump,:][:,::-1,:], output_shape=my_shape)(x)
+  flat = Flatten()(x)
   x = Dense(16, activation='relu')(flat)
-  x = Add()([Dense(alphabet_size)(x),  Dense(alphabet_size)(flat)])
-  x = Dense(alphabet_size)(x) 
+  x = Add(name='logits')([Dense(alphabet_size)(x),  Dense(alphabet_size)(flat)])
+
   s1 = Activation('softmax', name="1")(x)
   s2 = Activation('softmax', name="2")(x)
   s3 = Activation('softmax', name="3")(x)
+
+  model_prev = Model(inputs_bits, s1)
+  logits = model_prev.get_layer('logits').output
+  d = Dense(1024, activation='relu')(flat)
+  d = res_block(d, 1024, 'relu')
+  d = Dense(alphabet_size)(d)
+  next_layer = Add()([d, logits])
+  # next_layer = Dense(alphabet_size)(next_layer)
+  s1 = Activation('softmax', name="1")(next_layer)
+  # s2 = Activation('softmax', name="2")(x)
+  # s3 = Activation('softmax', name="3")(x)
 
   model = Model(inputs_bits, s1)
-  
-  return model
 
-def LSTMhybrid(bs,time_steps,alphabet_size):
-  inputs_bits = Input(shape=(time_steps,))
-  x = Embedding(alphabet_size, 8,)(inputs_bits)
-#   x = Bidirectional(CuDNNGRU(16, stateful=False, return_sequences=True))(x)
-  x = Flatten()(x)
-  x = Dense(256)(x)
-  x = res_block(x, 256, 'relu')
-  x = Dense(128)(x)
-  x = res_block(x, 128, 'relu')
-  x = res_block(x, 128, 'relu')
-  x = res_block(x, 128, 'relu')
-  x = BatchNormalization()(x)
-  x = Activation('relu')(x)
-  x = Dense(32)(x)
-
-  x = BatchNormalization()(x)
-  x = Activation('relu')(x)
-  x = Dense(alphabet_size)(x)
-
-  s1 = Activation('softmax', name="1")(x)
-  s2 = Activation('softmax', name="2")(x)
-  s3 = Activation('softmax', name="3")(x)
-
-  model = Model(inputs_bits, [s1, s2, s3])
-
-  return model
+  return model, model_prev
 
 print("Calling")
 sequence = np.load('output.npy')
@@ -228,16 +225,15 @@ X, Y = generate_single_output_data(sequence, batch_size, sequence_length)
 print(Y.shape[1])
 
 
-# model_teacher = resnet(batch_size, sequence_length, n_classes)
+model_student, model_teacher = biGRU_big(batch_size, sequence_length, n_classes)
 
-# optim = keras.optimizers.Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-# model_teacher.compile(loss=loss_fn, optimizer=optim, metrics=['acc'])
-# model_teacher.load_weights('model')
+optim = keras.optimizers.Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+model_teacher.compile(loss=loss_fn, optimizer=optim, metrics=['acc'])
+model_teacher.load_weights('directexp')
 
-model_student = biGRU_big(batch_size, sequence_length, n_classes)
-# for l in model_teacher.layers:
-#     l.trainable = False
+for l in model_teacher.layers:
+    l.trainable = True
 
-model_teacher=None
+model_student.summary()
 
 fit_model(X, Y, batch_size, num_epochs, model_student, model_teacher)
