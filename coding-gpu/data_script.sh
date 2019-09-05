5ebc4903eee6d65fded99d42242efe36ede2d16d
mohit1997 <goyal.mohit999@gmail.com>
2019-09-04 21:58:31 -0500

    clean2

---
+++ coding-gpu/data_script.sh
@@ -0,0 +1,17 @@
+# python generate_data.py --data_type 0entropy --markovity 10 --file_name files_to_be_compressed/xor10.txt
+# python generate_data.py --data_type 0entropy --markovity 20 --file_name files_to_be_compressed/xor20.txt
+# python generate_data.py --data_type 0entropy --markovity 30 --file_name files_to_be_compressed/xor30.txt
+# python generate_data.py --data_type 0entropy --markovity 40 --file_name files_to_be_compressed/xor40.txt
+# python generate_data.py --data_type 0entropy --markovity 50 --file_name files_to_be_compressed/xor50.txt
+# python generate_data.py --data_type 0entropy --markovity 60 --file_name files_to_be_compressed/xor60.txt
+
+# python generate_data.py --data_type iid --file_name files_to_be_compressed/iid.txt
+
+python generate_data.py --data_type HMM --markovity 10 --file_name files_to_be_compressed/hmm10.txt
+python generate_data.py --data_type HMM --markovity 20 --file_name files_to_be_compressed/hmm20.txt
+python generate_data.py --data_type HMM --markovity 30 --file_name files_to_be_compressed/hmm30.txt
+python generate_data.py --data_type HMM --markovity 40 --file_name files_to_be_compressed/hmm40.txt
+python generate_data.py --data_type HMM --markovity 50 --file_name files_to_be_compressed/hmm50.txt
+python generate_data.py --data_type HMM --markovity 60 --file_name files_to_be_compressed/hmm60.txt
+
+
