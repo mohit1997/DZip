0f1f5bfa703aec8e871742b6a35ef6c238a26cc3
mohit1997 <goyal.mohit999@gmail.com>
2019-09-04 21:57:45 -0500

    cleaned repo

--- DNA/get_data.sh
+++
@@ -1,3 +0,0 @@
-mkdir data
-wget 'ftp://hgdownload.cse.ucsc.edu/goldenPath/hg38/chromosomes/chr20.fa.gz' -O ./data/chr20.fa.gz
-gunzip ./data/chr20.fa.gz
--- text8/download.sh
+++
@@ -1,2 +0,0 @@
-wget http://mattmahoney.net/dc/text8.zip -P .
-unzip ./text8.zip
