9f17be4892e74dee1be72e80a173cef15ea39543
Mohit Goyal <mohit@idpg-serv-01.csl.illinois.edu>
2019-09-16 08:34:03 -0500

    moved

---
+++ fastq/quality/compress.sh
@@ -0,0 +1,15 @@
+FILE="first_400000"
+BASE=${FILE##*/}
+BASE=${BASE%.*}
+# echo $BASE
+PARAM_FILE=params_$BASE
+PRNN=biGRU_jump
+ARNN=biGRU_big
+JOINT=_
+echo $PARAM_FILE
+
+# python run.py --file_name $FILE
+# python train_PRNN.py --file_name $BASE --model $PRNN
+echo "$BASE$JOINT$PRNN"
+python train_PRNN.py --file_name $BASE --epochs 10 --gpu 0
+python train_ARNN.py --file_name $BASE --ARNN $ARNN --PRNN $PRNN --gpu 0
