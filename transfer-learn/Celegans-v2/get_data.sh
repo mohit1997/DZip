64765af43bb6b72e98b7ab7051e1da39052d34cc
Mohit Goyal <mohit@idpg-serv-01.csl.illinois.edu>
2019-10-22 21:54:13 -0500

    clean

--- transfer-learn/Celegans-v2/get_data.sh
+++
@@ -1,3 +0,0 @@
-mkdir data
-wget 'ftp://ftp.ensembl.org/pub/release-97/fasta/caenorhabditis_elegans/dna/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz' -O ./data/celegchr.fa.gz
-gunzip ./data/celegchr.fa.gz
