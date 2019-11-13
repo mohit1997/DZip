bafacefe6a6d2714a85b9d2d32294eadb4afe0b8
Mohit Goyal <mohit@idpg-serv-01.csl.illinois.edu>
2019-11-12 20:59:27 -0600

    added bootstrap only mode

--- encode-decode/compress.sh
+++ encode-decode/compress.sh
@@ -6,13 +6,19 @@ PARAM_FILE=params_$BASE
 PRNN=biGRU_jump
 ARNN=biGRU_big
 JOINT=_
+mode=$2
 echo $PARAM_FILE
 
 python run.py --file_name $FILE
 
 python train_bootstrap.py --file_name $BASE --model $PRNN
 echo "$BASE$JOINT$PRNN"
-python compressor.py -model $BASE$JOINT$PRNN -model_name $ARNN -batch_size 128 -data $BASE -data_params $PARAM_FILE -output $BASE
-python decompressor.py -model $BASE$JOINT$PRNN -model_name $ARNN -batch_size 128 -input $BASE -output decom$JOINT$BASE
+if [ "$mode" = com ] ; then
+	python compressor.py -model $BASE$JOINT$PRNN -model_name $ARNN -batch_size 128 -data $BASE -data_params $PARAM_FILE -output $BASE
+elif [ "$mode" = bs ] ; then
+	python compressor_bs.py -model $BASE$JOINT$PRNN -model_name $ARNN -batch_size 128 -data $BASE -data_params $PARAM_FILE -output $BASE
+fi
+
+# python decompressor.py -model $BASE$JOINT$PRNN -model_name $ARNN -batch_size 128 -input $BASE -output decom$JOINT$BASE
 
 
--- encode-decode/decompress.sh
+++ encode-decode/decompress.sh
@@ -6,7 +6,11 @@ PARAM_FILE=params_$BASE
 PRNN=biGRU_jump
 ARNN=biGRU_big
 JOINT=_
+mode=$2
 echo $PARAM_FILE
 
-python decompressor.py -model $BASE$JOINT$PRNN -model_name $ARNN -batch_size 128 -input $BASE -output decom$JOINT$BASE
-
+if [ "$mode" = com ] ; then
+	python decompressor.py -model $BASE$JOINT$PRNN -model_name $ARNN -batch_size 128 -input $BASE -output decom$JOINT$BASE
+elif [ "$mode" = bs ] ; then
+	python decompressor_bs.py -model $BASE$JOINT$PRNN -model_name $ARNN -batch_size 128 -input $BASE -output decom$JOINT$BASE
+fi
