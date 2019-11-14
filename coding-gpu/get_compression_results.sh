99534afabd4c6515134591a68e1b8e2a18828314
Mohit Goyal <mohit@idpg-serv-01.csl.illinois.edu>
2019-11-13 18:09:47 -0600

    default epochs 10

--- coding-gpu/get_compression_results.sh
+++ coding-gpu/get_compression_results.sh
@@ -8,5 +8,5 @@ JOINT=_
 
 python run.py --file_name $FILE
 
-python train_bootstrap.py --file_name $BASE --model $PRNN
+python train_bootstrap.py --file_name $BASE --model $PRNN --epochs 10
 python train_combined.py --file_name $BASE --PRNN $PRNN --ARNN $ARNN
